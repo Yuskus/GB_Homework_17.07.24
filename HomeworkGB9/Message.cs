dababd4e0e60b5f6c2d1459600184da881176df6
Yuskus <yuskus@yandex.ru>
2024-07-17 22:16:38 +0300

    Добавьте файлы проекта.

---
+++ HomeworkGB9/Message.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace HomeworkGB9
+{
+    public class Message
+    {
+        public string? Name { get; set; }
+        public string? Text { get; set; }
+        public DateTime Time { get; set; }
+        public Message() { }
+        public Message(string name, string text)
+        {
+            Name = name;
+            Text = text;
+            Time = DateTime.Now;
+        }
+
+        public string GetJson() => JsonSerializer.Serialize(this);
+        public static Message? GetMessage(string json) => JsonSerializer.Deserialize<Message>(json);
+        public override string ToString() => $"From: {Name}\nSent: {Time}\nText: {Text}";
+    }
+}
---
+++ HomeworkGB9/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HomeworkGB9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Chat.Server();
+            }
+            else
+            {
+                Chat.Client(args[0]);
+            }
+        }
+    }
+}
